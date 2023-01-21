773d020270decc8ee364300c9f3b4056d6e49bf1
Mac <mac.kittipat@gmail.com>
2023-01-21 13:13:18 +0700

    bootapi : Create Sample Product API with SpringBoot

---
+++ mac-bootapi/src/main/java/com/mackittipat/macbootapi/MacBootapiApplication.java
@@ -0,0 +1,13 @@
+package com.mackittipat.macbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacBootapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacBootapiApplication.class, args);
+	}
+
+}
---
+++ mac-bootapi/src/test/java/com/mackittipat/macbootapi/MacBootapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mackittipat.macbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacBootapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
